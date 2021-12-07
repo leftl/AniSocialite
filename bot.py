7e486e6c199d990198c371b7566db006cd8d1074
Tyler W <19383474+leftl@users.noreply.github.com>
2021-12-07 15:39:05 -0800

    refactor config and self userid

--- bot.py
+++ bot.py
@@ -8,11 +8,12 @@ from queries import request, GET_ACTIVITES, LIKE_ACTIVITY, GET_USER_ID
 API_URL = "https://graphql.anilist.co"
 ENV_PATH = Path(".env")
 SLEEP_TIME = 5
-UID = 0
 headers = { "Content-Type": "application/json",
             "Accept": "application/json" }
+config = None
 
 def init():
+    global config
     config = dotenv_values(ENV_PATH)
 
     if 'access_token' not in config.keys() or config['access_token'] == "":
@@ -24,10 +25,11 @@ def init():
     if 'user_id' not in config.keys() or config['user_id'] == "":
         resp = request(API_URL, headers, GET_USER_ID)
         set_key("./.env", "user_id", str(resp['data']['UpdateUser']['id']))
+        config = dotenv_values(ENV_PATH)
 
 def main():
     init()
-    config = dotenv_values(ENV_PATH)
+    userid = config['user_id'] if config is not None else 0
 
     # CONFIG: The ID of each series (Anime OR Manga) you wish to monitor the social activities of.
     #   Cowboy Bebop: https://anilist.co/anime/1/Cowboy-Bebop/ => ID: 1
@@ -35,7 +37,7 @@ def main():
     media_ids = [1, 530]
 
     while True:
-        resp = request(API_URL, headers, GET_ACTIVITES, variables={'media_ids' : media_ids, 'self_uid' : config['user_id']})
+        resp = request(API_URL, headers, GET_ACTIVITES, variables={'media_ids' : media_ids, 'self_uid' : userid})
 
         if resp is None:
             continue
